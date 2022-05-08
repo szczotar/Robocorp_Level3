f091ad52356eef4b68a4af348bb5fb3e9435b6f8
szczotar <you@example.com~>
2022-05-08 22:15:46 +0200

    Exceptions

--- consumer.py
+++ consumer.py
@@ -15,20 +15,20 @@ http = HTTP()
 tables = Tables()
 queue = WorkItems()
 os.chdir("output")
-logging.basicConfig(filename=f"{os.getcwd()}/logs.log",level=logging.DEBUG)
+#logging patternP
+logging.basicConfig(filename=f"{os.getcwd()}/logs.log",format='%(asctime)s %(message)s', datefmt ="%d-%m-%Y %H:%M:%S", 
+                    level=logging.INFO)
 
 class CoutryCodeError(Exception):
-     @staticmethod
-     def message():
-        message = " Country code is wrong"
-        return message
+    def __init__(self, message = None, errors = None):
+        super().__init__(message)
+        self.errors = errors
   
 class APIResposneError(Exception):
-    @staticmethod
-    def message():
-        message = " API internal serve error"
-        return message
- 
+    def __init__(self, message = None, errors = None):
+        super().__init__(message)
+        self.errors = errors
+
 def validate(payload):
     valid = len(payload["country"]) == 3 
     return valid
@@ -39,22 +39,22 @@ def Post_data(payload):
     return valid
 
 def Handle_businessException(err):
-    logging.error(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + err.message())
-    queue.release_input_work_item(state= State.FAILED, exception_type="BUSINESS", message= err.message())
+    # logging.error(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + err.message())
+    queue.release_input_work_item(state= State.FAILED, exception_type="BUSINESS", message= "Country code is wrong")
 
 def Handle_APIException(err):
-    logging.error(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + err.message())
-    queue.release_input_work_item(state= State.FAILED, exception_type="APPLICATION", message= err.message())
+    # logging.error(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + err.message())
+    queue.release_input_work_item(state= State.FAILED, exception_type="APPLICATION", message= "API internal serve error")
 
 def Release_WorkItem():
-    logging.info(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " item done")
-    queue.release_input_work_item(state= State.DONE, exception_type="APPLICATION", message= " item done")
+    # logging.info(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " item done")
+    queue.release_input_work_item(state= State.DONE, exception_type="APPLICATION", message= "item done")
 
 def Process_data():
     try:
         payload = queue.get_work_item_payload()
-        if not validate(payload): raise CoutryCodeError()
-        if not Post_data(payload): raise APIResposneError()
+        if not validate(payload): raise CoutryCodeError("Country code is wrong")
+        if not Post_data(payload): raise APIResposneError("API internal serve error")
         Release_WorkItem()
    
     except CoutryCodeError as err:
--- producer.py
+++ producer.py
@@ -1,3 +1,4 @@
+from json.tool import main
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables
@@ -9,8 +10,12 @@ http = HTTP()
 json = JSON()
 tables = Tables()
 queue = WorkItems()
-
+import logging
 os.chdir("output")
+logging.basicConfig(filename=f"{os.getcwd()}/logs.log",format='%(asctime)s %(message)s', datefmt ="%d-%m-%Y %H:%M:%S", 
+                    level=logging.INFO)
+
+
 r = requests.get("https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json")
 data_json = r.json()
 table = pd.DataFrame(data_json["value"])
@@ -19,14 +24,21 @@ sorted_table = table.loc[(table['NumericValue'] < 5.0) & (table['Dim1'] == "BTSX
 sorted_table.to_excel(f"{os.getcwd()}/trafic.xlsx", index = False)
 queue.get_input_work_item()
 
-for index,row in enumerate(sorted_table.index):
-    queue.create_output_work_item()
-    queue.set_work_item_variable("country",sorted_table.loc[row,'SpatialDim'])
-    queue.set_work_item_variable("year", str(sorted_table.loc[row,'TimeDim']))
-    queue.set_work_item_variable("rate",str(sorted_table.loc[row,'NumericValue']))
-    queue.save_work_item()
-    if index == 99:
-        break
+
+def populate_queue():
+    for index,row in enumerate(sorted_table.index):
+        queue.create_output_work_item()
+        queue.set_work_item_variable("country",sorted_table.loc[row,'SpatialDim'])
+        queue.set_work_item_variable("year", str(sorted_table.loc[row,'TimeDim']))
+        queue.set_work_item_variable("rate",str(sorted_table.loc[row,'NumericValue']))
+        queue.save_work_item()
+        logging.info("work item saved")
+        if index == 99:
+            break
+
+if __name__ == "__main__":
+    populate_queue()
+
 
 # http.download("https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json", f"{os.getcwd()}/trafic.json", overwrite=True)
 # data_json = json.load_json_from_file(f"{os.getcwd()}/trafic.json")
